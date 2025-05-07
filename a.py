1b563580ef9f859e53d56eab1bdbd24f40e24e58
vallabh9 <vallabhgargade7181@gmail.com>
2025-05-07 06:29:49 +0530

    Update a.py

--- a.py
+++ a.py
@@ -1,1 +1,12 @@
-print('hello')
+#print('hello')
+
+# Ask the user to enter a number
+num = int(input("Enter a number: "))
+
+# Check if the number is even or odd
+if num % 2 == 0:
+    print(f"{num} is Even")
+else:
+    print(f"{num} is Odd")
+
+
